53675a19b83d39a1b88d4021c13217025ef550d2
alex-yellow <jeltovas91@gmail.com>
2025-05-30 15:05:11 +0300

    add model Task

---
+++ src/main/java/com/example/springcrud/model/Task.java
@@ -0,0 +1,63 @@
+package com.example.springcrud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+    public Task(){
+
+    }
+
+    public Task(String title, String description, boolean published){
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public boolean isPublished(){
+        return published;
+    }
+
+    public void setPublished(boolean published){
+        this.published = published;
+    }
+
+    @Override
+    public String toString(){
+        return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
+}
